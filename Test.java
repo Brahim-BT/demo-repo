e150231e107cf1a354413bb29a13456ecd2da9d7
Brahim Baibbat <baybat7@gmail.com>
2021-02-23 18:41:55 +0100

    Added Test.java
    Attempting to add a file to my repository

--- Test.java
+++ Test.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        double d = Math.random() * 100;
+        int n = (int) d;
+        System.out.println("Try to guess the number :");
+        System.out.print("Guess the number : ");
+        int g = sc.nextInt();
+        while (g != n) {
+            if (g > n) {
+                System.out.println("Smaller than that");
+                g = sc.nextInt();
+            } else if (g < n) {
+                System.out.println("Bigger than that");
+                g = sc.nextInt();
+            } else {
+                break;
+            }
+        }
+        System.out.println("Bingoo !!");
+        sc.close();
     }
 }
